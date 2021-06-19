c5866a660c4542a16b9c6d789ef9d77a96fb0587
raghav1674 <raghav.81-cse-17@mietjammu.in>
2021-06-19 14:03:59 +0530

    Done all questions till Day 2

--- Recursion/05/StairCaseTraversal.py
+++ Recursion/05/StairCaseTraversal.py
@@ -1,3 +1,12 @@
-def max_ways_to_reach_staircase_end(staircase_height,max_step):
-        
-        
+
+
+def max_ways_to_reach_staircase_end(staircase_height, max_step, current_step=1):
+
+    if staircase_height == 0 or current_step == 0:
+        return 1
+    elif staircase_height >= current_step:
+        return max_ways_to_reach_staircase_end(
+            staircase_height-current_step, max_step, current_step-1) + staircase_height//current_step
+
+
+print(max_ways_to_reach_staircase_end(10, 2))
